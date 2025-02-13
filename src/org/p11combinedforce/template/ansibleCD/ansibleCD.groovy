ac4eb6f4ebb979eaa99d66d5fa113b5793e48dc1
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-13 23:55:16 +0530

    Create ansibleCD.groovy

---
+++ src/org/p11combinedforce/template/ansibleCD/ansibleCD.groovy
@@ -0,0 +1,17 @@
+package org.p11combinedforce.ansibleCD
+
+import org.p11combinedforce.ansibleCD.*
+import org.p11combinedforce.common.*
+
+def call(String url, String branch, String creds, String playbookName, String inventoryPath, String playbookPath) {
+
+    // Create instances of required classes
+     def gitCheckOut = new gitCheckOut()
+     def wsClean=new wsClean()
+     def RoleExecution = new RoleExecution()
+
+    // Call methods with correct arguments
+    wsClean.call()
+    gitCheckOut.call(url, branch, creds)  
+    def RoleExecution.call(inventoryPath, playbookPath)
+}
