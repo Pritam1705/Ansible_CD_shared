5b857b05d6fcb7dbd29bc5e5eb663dc8ebb9d637
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-02-14 10:20:46 +0530

    Update ansibleCD.groovy

--- src/org/p11combinedforce/template/ansibleCD/ansibleCD.groovy
+++ src/org/p11combinedforce/template/ansibleCD/ansibleCD.groovy
@@ -3,7 +3,7 @@ package org.p11combinedforce.ansibleCD
 import org.p11combinedforce.ansibleCD.*
 import org.p11combinedforce.common.*
 
-def call(String url, String branch, String creds, String playbookName, String inventoryPath, String playbookPath) {
+def call(String url, String branch, String creds, String inventoryPath, String playbookPath) {
 
     // Create instances of required classes
      def gitCheckOut = new gitCheckOut()
